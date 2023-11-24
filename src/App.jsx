ccd89982fa0cc12136ccf5c52950a7397f40baff
Ethereal49 <128294874+Ethereal49@users.noreply.github.com>
2023-11-24 12:54:49 +0000

    edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ethereal",
+  title: "WStudent",
+  email: "ethereal49@outlook.com",
+  gitHub: "null",
+  instagram: "null",
+  linkedIn: "null",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "null",
+  youTube: "null",
 };
 
 const primaryColor = "#4E567E";
